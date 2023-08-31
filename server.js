73119be396ffd85db37dd61269266516b95ea488
joe-shajan <joeshajan1551@gmail.com>
2023-08-31 12:39:05 +0530

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80; // You can choose any available port
+const port = 8080; // You can choose any available port
 
 // Define a route
 app.get("/", (req, res) => {
