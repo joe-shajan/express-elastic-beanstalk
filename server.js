519891a47e47fd6837eab993aedfb2575cf3b997
joe-shajan <joeshajan1551@gmail.com>
2023-09-01 16:38:02 +0530

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8080; // You can choose any available port
 
 // Define a route
 app.get("/", (req, res) => {
-  res.send("Hello, Express running with elastic beanstalk!");
+  res.send("Hello, Express running with elastic beanstalk updated!");
 });
 
 // Start the server
